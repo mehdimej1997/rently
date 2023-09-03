de596ebf8f06cafe6e1c6a37c562da1b9592bb95
Mejjad El Mehdi <mejjad@moenn-technologies.com>
2023-09-03 09:37:41 +0100

    initial commit

---
+++ rently/rently-api/src/main/java/com/rently/RentlyApplication.java
@@ -0,0 +1,13 @@
+package com.rently;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentlyApplication.class, args);
+	}
+
+}
